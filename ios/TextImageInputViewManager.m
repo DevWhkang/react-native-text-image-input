cb45a029967a41b4bb22eaa80b45d559acd76fac
DevWhkang <devwhkang@gmail.com>
2024-10-29 18:21:57 +0900

    add. init config

--- ios/TextImageInputViewManager.m
+++ ios/TextImageInputViewManager.m
@@ -3,5 +3,8 @@
 @interface RCT_EXTERN_MODULE(TextImageInputViewManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(color, NSString)
+RCT_EXPORT_VIEW_PROPERTY(fontSize, NSNumber)
+
+RCT_EXTERN_METHOD(insertImage:(NSString *)imageUrl)
 
 @end
